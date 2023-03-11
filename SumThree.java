6552a89bf50be989be697ea8dc4e41265548729a
daya8789 <114335626+daya8789@users.noreply.github.com>
2023-03-11 22:42:53 +0530

    Add files via upload

---
+++ SumThree.java
@@ -0,0 +1,14 @@
+
+public class SumThree {
+
+	public static void main(String[] args) {
+		System.out.print("The sum of three number is : ");
+		int num1 = 34;
+		int num2 = 45;
+		int num3 = 23;
+		int sum = num1 + num2 + num3;
+		System.out.println(sum);
+
+	}
+
+}
