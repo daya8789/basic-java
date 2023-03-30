ea804c2533e3e49bf6e22930717b6109f97a9c87
Dayashankar Yadav <114335626+daya8789@users.noreply.github.com>
2023-03-31 00:01:20 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,18 @@
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		int a=12;
+		
+		System.out.println(a++);
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a);
+		System.out.println(a--);
+		System.out.println(a);
+		System.out.println(--a);
+		System.out.println(a);
+
+	}
+
+}
