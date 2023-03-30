62cc9827293f757486fe9e777a3f7f02800f9373
Dayashankar Yadav <114335626+daya8789@users.noreply.github.com>
2023-03-30 23:48:38 +0530

    Add files via upload

---
+++ PracticeSet_5.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class PracticeSet_5 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// Question no. 1
+		/*int n=4;
+		for(int i=n; i>0; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}*/
+		
+		//		Question no. 2
+		
+		/*int sum=0;
+		int n=5;
+		for(int i=0; i<n; i++) {
+			sum = sum + (2*i);
+		}
+		System.out.print("The sum of n even number is : ");
+		System.out.println(sum);*/
+		
+		//		Question no. 3
+//		int n;
+//		System.out.println("Enter the number:");
+//		n = sc.nextInt();
+//		System.out.println("The table of "+n+" is : ");
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(n+" X " +i+" = "+ n*i);
+//		}
+		
+		// Question no. 4
+//		int n;
+//		System.out.println("Enter the Number :");
+//		n = sc.nextInt();
+//		for(int i=10; i>=1; i--) {
+//			System.out.println(n+" X "+i+" = "+n*i);
+//		}
+		
+		//	Question no. 5
+//		int n, Factorial=1;
+//		n=sc.nextInt();
+//		System.out.print("The Factorial of "+n+" is : ");
+//		for(int i=1; i<=n; i++) {
+//			Factorial *= i;
+//		}
+//		System.out.println(Factorial);
+		
+		// Question No. 6
+//		int n, factorial=1;
+//		System.out.print("Enter the Number which you want to get Factorial : ");
+//		n=sc.nextInt();
+//		int i = 1;
+//		while(i<=n) {
+//			factorial *= i;
+//			i++;
+//		}
+//		System.out.println(factorial);
+		
+		//	Question No. 7
+//		int n, i;
+//		System.out.print("Enter the No. of stars : ");
+//		n=sc.nextInt();
+//		for(i=n; i>0; i--) {
+//			for(int j=1;j<i;j++) {
+//			System.out.print("*");
+//			}
+//			System.out.print("\n");
+//		}
+		
+		//	Question No. 8
+		int n=5, i, j;
+		
+	}
+
+}
